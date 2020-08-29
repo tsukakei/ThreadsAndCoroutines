8c3381a0a49add2ca6882de94c0a0454cd34f1ff
Keita Tsukamoto <keita.tsukamoto@yesod.co>
2020-08-29 11:23:41 +0900

    Thread poolの例を作成

---
+++ src/main/kotlin/ThreadPoolSample.kt
@@ -0,0 +1,23 @@
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    // Try setting this to 1, number of cores, 100, 2000, 3000, and see what happens
+    val pool = Executors.newFixedThreadPool(4)
+    val counter = AtomicInteger(0)
+    val start = System.currentTimeMillis()
+    for (i in 1..10_000) {
+        pool.submit {
+            counter.incrementAndGet()
+            Thread.sleep(100)
+            counter.incrementAndGet()
+        }
+    }
+    pool.awaitTermination(10, TimeUnit.SECONDS)
+    pool.shutdown()
+    println("Took me ${System.currentTimeMillis() - start} millis to complete ${counter.get() / 2} tasks")
+}
