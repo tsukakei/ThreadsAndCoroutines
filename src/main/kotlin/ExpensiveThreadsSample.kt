cb8a3a35e3a3ccd81071809007d1236f87052dc5
Keita Tsukamoto <keita.tsukamoto@yesod.co>
2020-08-29 11:15:44 +0900

    Thread落ちのサンプルを追加

---
+++ src/main/kotlin/ExpensiveThreadsSample.kt
@@ -0,0 +1,19 @@
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    val counter = AtomicInteger()
+    try {
+        for (i in 0..100_000) {
+            thread {
+                counter.incrementAndGet()
+                Thread.sleep(1000)
+            }
+        }
+    } catch (oome: OutOfMemoryError) {
+        println("Spawned ${counter.get()} threads before crashing")
+        exitProcess(-42)
+    }
+}
